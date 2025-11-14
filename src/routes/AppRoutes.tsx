bb57e82cdb32f28ad50fe2bf8b7732b35131325a
Duartegdm <gduarte.study@gmail.com>
2025-11-14 20:44:47 -0300

    feat: criando rota para a página de integrantes

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import PaginaInicial from "../pages/PaginaInicial";
+import Integrantes from "../pages/Integrantes";
 
 export const routes = createBrowserRouter([
     {
         path: "/",
         element: <PaginaInicial />
+    },
+    {
+        path: "/integrantes",
+        element: <Integrantes />
     }
 ])
