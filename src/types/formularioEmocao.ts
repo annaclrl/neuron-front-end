c9103f8a6ec3e6c8c8d4e775869c1e33910bb855
Anna <annaclararussoluca@gmail.com>
2025-11-15 00:13:48 -0300

    feat: criando types para formularioEmocao

---
+++ src/types/formularioEmocao.ts
@@ -0,0 +1,18 @@
+export type FormInputs = {
+  idEmocao: number;
+  intensidade: number;
+  descricao: string;
+  motivacao: number;
+  felicidade: number;
+  estresse: number;
+  saudeMental: number;
+  problemas: number;
+  modoVer: number;
+  observacaoGeral: string;
+};
+
+export type DicaEmocao = {
+  emocao: string;
+  dica: string;
+  cor: string;
+};
