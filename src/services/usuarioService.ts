68004e1b6ddff41781d501e812d0f2bda5ae81c0
Tiagozguedes <tiagozguedes@gmail.com>
2025-11-19 23:19:51 -0300

    feat: criando métodos de atualizar e deletar usuario

--- src/services/usuarioService.ts
+++ src/services/usuarioService.ts
@@ -1,10 +1,11 @@
-import type { CadastroForm } from "../types/usuario";
+import type { AtualizarUsuarioDto, CadastroForm } from "../types/usuario";
 
 const mapTipoUsuario = {
   RH_CLEVEL: 1,
   GESTOR: 2,
   FUNCIONARIO: 3,
 } as const;
+
 
 export async function cadastrarUsuario(data: CadastroForm): Promise<void> {
 
@@ -54,17 +55,61 @@ export async function cadastrarUsuario(data: CadastroForm): Promise<void> {
 
 
 export async function buscarUsuarioPorId(id: number) {
-    const token = localStorage.getItem('token');
+  const token = localStorage.getItem('token');
 
-    const response = await fetch(`http://localhost:8080/usuarios/${id}`, {
-        headers: {
-            'Authorization': `Bearer ${token}`,
-        },
-    });
+  const response = await fetch(`http://localhost:8080/usuarios/${id}`, {
+    headers: {
+      'Authorization': `Bearer ${token}`,
+    },
+  });
 
-    if (!response.ok) {
-        throw new Error('Erro ao obter os dados do usuario.');
-    }
+  if (!response.ok) {
+    throw new Error('Erro ao obter os dados do usuario.');
+  }
 
-    return await response.json();
+  return await response.json();
 };
+
+export async function atualizarUsuario(id: number, data: AtualizarUsuarioDto) {
+  const token = localStorage.getItem("token");
+
+  const agora = new Date().toISOString().slice(0, -1);
+
+  const body = { 
+    ...data,        
+    dataAtualizacao: agora,
+    dataCadastro: data.dataCadastro 
+  };
+
+  const resp = await fetch(`http://localhost:8080/usuarios/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`
+    },
+    body: JSON.stringify(body),
+  });
+
+  if (!resp.ok) {
+    throw new Error("Erro ao atualizar o usuário");
+  }
+
+  return;
+}
+
+export async function deletarUsuario(id: number) {
+  const token = localStorage.getItem("token");
+
+  const resp = await fetch(`http://localhost:8080/usuarios/id/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Authorization": `Bearer ${token}`
+    }
+  });
+
+  if (!resp.ok) {
+    throw new Error("Erro ao desativar o usuário");
+  }
+}
+
+
