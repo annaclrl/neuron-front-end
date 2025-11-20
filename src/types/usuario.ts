81a014465bf230a7ce1289c0cf39e565de52bd32
Tiagozguedes <tiagozguedes@gmail.com>
2025-11-19 23:19:25 -0300

    feat: criando interface para AtualizarUsuarioDto

--- src/types/usuario.ts
+++ src/types/usuario.ts
@@ -3,8 +3,9 @@ export type TipoUsuario = 'RH_CLEVEL' | 'GESTOR' | 'FUNCIONARIO';
 export type DadosPessoais = {
   nome: string;
   email: string;
-  departamento : string; 
+  departamento: string; 
   status: string;
+  senha: string;
 };
 
 export type DadosLogin = {
@@ -21,13 +22,30 @@ export type CadastroForm =
   DadosLogin &
   DadosAdministrativos;
 
-
 export type UsuarioComLogin =
   Pick<CadastroForm, "email" | "senha">;
 
 
-export type Usuario =
-  Pick<DadosPessoais, "nome" | "email"> & {
-    tipo: TipoUsuario;
-  };
+export type Usuario = {
+  id?: number;
+  nome: string;
+  email: string;
+  senha: string;
+  status: string; 
+  dataCadastro: string;
+  codigoAcesso: number;
+  codigoDepartamento: number;
+  tipo?: string;
+};
+
+export interface AtualizarUsuarioDto {
+  nome: string;
+  email: string;
+  senha: string;
+  codigoAcesso: number;
+  codigoDepartamento: number;
+  status: string;
+  dataCadastro: string;
+}
+
 
